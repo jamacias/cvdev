fdaa9c8d9557bcb8253b62386d371884404f3df5
Javier Macias Sola <jamaciastf@gmail.com>
2026-03-02 23:07:41 +0100

    Use RigidTransformation which is more stable over time

--- src/traits/traits.h
+++ src/traits/traits.h
@@ -1,11 +1,11 @@
 #ifndef TRAITS_TRAITS_H
 #define TRAITS_TRAITS_H
 
-#include <Magnum/SceneGraph/MatrixTransformation3D.h>
+#include <Magnum/SceneGraph/RigidMatrixTransformation3D.h>
 #include <Magnum/SceneGraph/Scene.h>
 
 using namespace Magnum;
-typedef SceneGraph::Object<SceneGraph::MatrixTransformation3D> Object3D;
-typedef SceneGraph::Scene<SceneGraph::MatrixTransformation3D>  Scene3D;
+typedef SceneGraph::Object<SceneGraph::RigidMatrixTransformation3D> Object3D;
+typedef SceneGraph::Scene<SceneGraph::RigidMatrixTransformation3D>  Scene3D;
 
 #endif // TRAITS_TRAITS_H
